0bec1e88504b2fe27506531b779bf3510cfbeba3
berdmival <mtsby7049874@gmail.com>
2019-11-16 21:29:36 +0300

    reformat and cleanup code

--- src/main/java/by/berdmival/bookstore/configuration/security/ResourceServerTokenStoreConfig.java
+++ src/main/java/by/berdmival/bookstore/configuration/security/ResourceServerTokenStoreConfig.java
@@ -3,13 +3,8 @@ package by.berdmival.bookstore.configuration.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class ResourceServerTokenStoreConfig {
