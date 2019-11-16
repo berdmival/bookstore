03ed6140631458b81328cc59224dfa31dd158985
berdmival <mtsby7049874@gmail.com>
2019-11-16 21:33:36 +0300

    fix resource server config

--- src/main/java/by/berdmival/bookstore/configuration/security/ResourceServerConfig.java
+++ src/main/java/by/berdmival/bookstore/configuration/security/ResourceServerConfig.java
@@ -2,7 +2,6 @@ package by.berdmival.bookstore.configuration.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -23,11 +22,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/**").access("#oauth2.hasScope('read')")
-                .and()
-                .headers().addHeaderWriter((request, response) -> {
+        http.headers().addHeaderWriter((request, response) -> {
             response.addHeader("Access-Control-Allow-Origin", "*");
             if (request.getMethod().equals("OPTIONS")) {
                 response.setHeader("Access-Control-Allow-Methods", request.getHeader("Access-Control-Request-Method"));
