9702cd2a051ea232140fd60d17328d3fb63a9749
berdmival <mtsby7049874@gmail.com>
2019-11-23 16:10:33 +0300

    remove commented code

--- src/main/java/by/berdmival/bookstore/configuration/security/AuthServerConfiguration.java
+++ src/main/java/by/berdmival/bookstore/configuration/security/AuthServerConfiguration.java
@@ -1,7 +1,6 @@
 package by.berdmival.bookstore.configuration.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -9,19 +8,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
-
 
 @Configuration
 public class AuthServerConfiguration extends AuthorizationServerConfigurerAdapter {
@@ -60,22 +55,6 @@ public class AuthServerConfiguration extends AuthorizationServerConfigurerAdapte
         oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
         oauthServer.allowFormAuthenticationForClients();
     }
-
-//    @Value("${spring.security.oauth2.client.registration.okta.client-id}")
-//    String clientId;
-//    @Value("${spring.security.oauth2.client.registration.okta.client-secret}")
-//    String clientSecret;
-//
-//    @Primary
-//    @Bean(name = "remoteTokenServices")
-//    public ResourceServerTokenServices remoteTokenServices() {
-//        RemoteTokenServices tokenService = new RemoteTokenServices();
-//        tokenService.setCheckTokenEndpointUrl(
-//                "http://localhost:7777/oauth/check_token");
-//        tokenService.setClientId(clientId);
-//        tokenService.setClientSecret(clientSecret);
-//        return tokenService;
-//    }
 
     @Primary
     @Bean
